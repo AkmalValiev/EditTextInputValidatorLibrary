410db8b0aa5abd0a88f366c4bcd27a1bd37780de
AkmalValiev <akki77777@mail.ru>
2022-04-22 21:43:35 +0500

    change library

--- InputValidator/src/main/java/uz/evkalipt/inputvalidator/InputValidator.kt
+++ InputValidator/src/main/java/uz/evkalipt/inputvalidator/InputValidator.kt
@@ -5,12 +5,12 @@ import androidx.core.widget.addTextChangedListener
 
 object InputValidator{
 
-    fun validator(str1:String, editText: EditText){
-        editText.addTextChangedListener {
-            if (str1!=editText.text.toString()){
-                editText.setCompoundDrawablesWithIntrinsicBounds(0,0, R.drawable.ic_baseline_info_24, 0)
-            }else if (str1==editText.text.toString()){
-                editText.setCompoundDrawablesWithIntrinsicBounds(0,0, R.drawable.ic_baseline_check_24, 0)
+    fun validator(editText1: EditText, editText2: EditText){
+        editText2.addTextChangedListener {
+            if (editText1.text.toString()!=editText2.text.toString()){
+                editText2.setCompoundDrawablesWithIntrinsicBounds(0,0, R.drawable.ic_baseline_info_24, 0)
+            }else if (editText1.text.toString()==editText2.text.toString()){
+                editText2.setCompoundDrawablesWithIntrinsicBounds(0,0, R.drawable.ic_baseline_check_24, 0)
             }
         }
     }
